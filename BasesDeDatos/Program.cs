e9b5e6b0d177807fe20ad3a359da4bd5541b87f5
Nivek258 <kevin_713@hotmail.com>
2015-03-09 23:13:04 -0600

    Sin asdf

--- BasesDeDatos/Program.cs
+++ BasesDeDatos/Program.cs
@@ -9,7 +9,7 @@ namespace BasesDeDatos
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application. asdf
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
