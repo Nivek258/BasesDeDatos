05e030c45833c79f7f1f481dbaeda8bf6d2e7543
ernestoivan22 <ernestoivan22@hotmail.com>
2015-03-09 23:09:29 -0600

    cambio asdf

--- BasesDeDatos/Program.cs
+++ BasesDeDatos/Program.cs
@@ -9,7 +9,7 @@ namespace BasesDeDatos
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. asdf
         /// </summary>
         [STAThread]
         static void Main()
