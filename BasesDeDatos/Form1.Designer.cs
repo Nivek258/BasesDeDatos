6fa794ad7a23733645a13b8062d482e4bf993a5b
ernestoivan22 <ernestoivan22@hotmail.com>
2015-03-13 00:41:40 -0600

    con Interfaz

--- BasesDeDatos/Form1.Designer.cs
+++ BasesDeDatos/Form1.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabEditor = new System.Windows.Forms.TabPage();
+            this.tabErrores = new System.Windows.Forms.TabPage();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.tbEditor = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabEditor.SuspendLayout();
+            this.tabErrores.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabEditor);
+            this.tabControl1.Controls.Add(this.tabErrores);
+            this.tabControl1.Location = new System.Drawing.Point(0, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(490, 271);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabEditor
+            // 
+            this.tabEditor.Controls.Add(this.tbEditor);
+            this.tabEditor.Location = new System.Drawing.Point(4, 22);
+            this.tabEditor.Name = "tabEditor";
+            this.tabEditor.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEditor.Size = new System.Drawing.Size(482, 245);
+            this.tabEditor.TabIndex = 0;
+            this.tabEditor.Text = "Editor";
+            this.tabEditor.UseVisualStyleBackColor = true;
+            // 
+            // tabErrores
+            // 
+            this.tabErrores.Controls.Add(this.textBox1);
+            this.tabErrores.Location = new System.Drawing.Point(4, 22);
+            this.tabErrores.Name = "tabErrores";
+            this.tabErrores.Padding = new System.Windows.Forms.Padding(3);
+            this.tabErrores.Size = new System.Drawing.Size(482, 245);
+            this.tabErrores.TabIndex = 1;
+            this.tabErrores.Text = "Errores";
+            this.tabErrores.UseVisualStyleBackColor = true;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCargar.Location = new System.Drawing.Point(116, 7);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnGuardar.Location = new System.Drawing.Point(207, 7);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnEjecutar.Location = new System.Drawing.Point(301, 7);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnEjecutar.TabIndex = 2;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            // 
+            // tbEditor
+            // 
+            this.tbEditor.AcceptsTab = true;
+            this.tbEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbEditor.Location = new System.Drawing.Point(0, 0);
+            this.tbEditor.Multiline = true;
+            this.tbEditor.Name = "tbEditor";
+            this.tbEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbEditor.Size = new System.Drawing.Size(482, 245);
+            this.tbEditor.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(-4, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(486, 249);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 309);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabEditor.ResumeLayout(false);
+            this.tabEditor.PerformLayout();
+            this.tabErrores.ResumeLayout(false);
+            this.tabErrores.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabEditor;
+        private System.Windows.Forms.TabPage tabErrores;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.TextBox tbEditor;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
