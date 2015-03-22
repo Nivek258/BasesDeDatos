2c4f37b77d8d1e5cd3b9a9e0e904ba5ccdc4dc2f
Nivek258 <kevin_713@hotmail.com>
2015-03-22 01:14:09 -0600

    con tabla semi

--- BasesDeDatos/ControlDirectorios.cs
+++ BasesDeDatos/ControlDirectorios.cs
@@ -9,7 +9,7 @@ namespace BasesDeDatos
 {
     class ControlDirectorios
     {
-
+        String DBactual = "";
         public void inicializar()
         {
             if (File.Exists("..\\DataDB\\archivoM.dat") == false)
@@ -39,7 +39,32 @@ namespace BasesDeDatos
         {
             File.AppendAllText("..\\DataDB\\archivoM.dat", "name: "+nombreDB +" int: 0"+Environment.NewLine);
             Directory.CreateDirectory("..\\DataDB\\"+nombreDB);
+            FileStream fs = File.Create("..\\DataDB\\" + nombreDB+"\\controlTablas.dat");
         }
-        
+
+        public Boolean existeTabla(String nombreTabla)
+        {
+            String contenido = "";
+            StreamReader myWriter = new StreamReader("..\\DataDB\\" + DBactual + "\\controlTablas.dat");
+            contenido += myWriter.ReadToEnd();
+            myWriter.Close();
+            if (contenido.Contains(nombreTabla))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+        public void agregarTabla(String nombreTabla, int cantidadRegs, List<Columna> datosColumnas)
+        {
+            String textoAppend = "name: " + nombreTabla + " registros: 0 columnas: [" ;
+            for(int i=0; i<datosColumnas.Count;i++){
+                textoAppend+= "[";
+                for(int j=0; j<datosColumnas)
+            }
+            File.AppendAllText("..\\DataDB\\" + DBactual + "\\controlTablas.dat", ) ;
+        }
     }
 }
