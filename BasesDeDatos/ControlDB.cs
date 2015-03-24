a646f2ad010fb9fabc6d6322ff9f79a5a866c0ec
ernestoivan22 <ernestoivan22@hotmail.com>
2015-03-24 16:46:15 -0600

    Agregadas Clases DataBase y ControlDB

---
+++ BasesDeDatos/ControlDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasesDeDatos
+{
+    class ControlDB
+    {
+        List<DataBase> listaDB;
+
+        public void agregarDataBase(DataBase nuevaDB)
+        {
+            listaDB.Add(nuevaDB);
+        }
+
+        public Boolean existeDataBase(String nombreDataBase)
+        {
+            for (int i = 0; i < listaDB.Count; i++)
+            {
+                if (listaDB[i].getNombre().Equals(nombreDataBase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void removerDataBase(String nombreDataBase)
+        {
+            for (int i = 0; i < listaDB.Count; i++)
+            {
+                if (listaDB[i].getNombre().Equals(nombreDataBase))
+                {
+                    listaDB.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ BasesDeDatos/DataBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasesDeDatos
+{
+    class DataBase
+    {
+        int numTablas;
+        String nombre;
+        
+        DataBase(String nombre, int numTablas){
+            this.nombre = nombre;
+            this.numTablas = numTablas;
+        }
+
+        public int getNumTablas()
+        {
+            return this.numTablas;
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setNumTablas(int numTablas)
+        {
+            this.numTablas = numTablas;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
